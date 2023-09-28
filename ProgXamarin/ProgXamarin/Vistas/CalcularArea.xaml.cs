ed239cad77aa9288c84f93d1d7ad036096cbe929
duran.valentina@hotmail.com <duran.valentina@hotmail.com>
2023-09-28 11:34:22 -0300

    Agregar archivos de proyecto.

---
+++ ProgXamarin/ProgXamarin/Vistas/CalcularArea.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProgXamarin.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalcularArea : ContentPage
+    {
+        decimal a;
+        decimal b;
+        decimal c;
+        decimal basexaltura;
+        public CalcularArea()
+        {
+            InitializeComponent();
+        }
+        private void Calcular() 
+        {
+            a = Convert.ToDecimal(Altura.Text);
+            b = Convert.ToDecimal(Base.Text);
+            c = a * b;
+            basexaltura = c / 2;
+            lblresultado.Text = basexaltura.ToString();
+
+        }
+        private void Validar()
+        {
+            if(!string.IsNullOrEmpty(Altura.Text))
+            {
+                Calcular();
+            }
+        }
+
+        private void btncalculararea_Clicked(object sender, EventArgs e)
+        {
+            Calcular();
+        }
+    }
+}
